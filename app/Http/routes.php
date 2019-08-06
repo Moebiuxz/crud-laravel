97c0a73d707f9e350ee8d273614bf39a830853a6
Álvaro Bunster <al.bunstr@gmail.com>
2019-08-06 17:28:35 -0400

    Se hicieron pruebas de rutas (GET).

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,25 @@
 |
 */
 
+// Ruta sin parámetro
+Route::get('prueba', function () {
+    return "Hola desde routes.php";
+});
+
+// Ruta con parámetro (get)
+Route::get('nombre/{nombre}', function ($nombre) {
+    return "Mi nombre es: ". $nombre;
+});
+
+// Ruta con parámetro con valor por defecto (nótese el ?)
+Route::get('nombre2/{nombre?}', function ($nombre = 'Alvaro') {
+    return "Mi nombre es: ". $nombre;
+});
+
+Route::get('edad/{edad}', function ($edad) {
+    return "Mi edad es: ". $edad;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
